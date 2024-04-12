88045aa2239fbe26c3bade73f325f77137150f86
Joey Wagner <joeyawagner22@gmail.com>
2024-04-12 19:47:08 -0400

    Update lr_model.py

--- api/lr_model.py
+++ api/lr_model.py
@@ -2,8 +2,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from pred_methods import lrPrediction, standardize_inputs, standardize_input
-
 
 bj = pd.read_csv('bj_dataset.csv')
 
@@ -13,7 +11,10 @@ y = bj['move']
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
+X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                    y, 
+                                                    test_size=0.2, 
+                                                    random_state=101)
 
 LRModel = LogisticRegression()
 LRModel.fit(X_train, y_train)
