f83eac882dc4923468610dd445705b4c06a9ea97
Joey Wagner <joeyawagner22@gmail.com>
2024-04-12 19:55:45 -0400

    Update knn_model.py

--- api/knn_model.py
+++ api/knn_model.py
@@ -2,7 +2,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from pred_methods import knnPrediction, standardize_inputs, standardize_input
 
 
 bj = pd.read_csv('bj_dataset.csv')
@@ -13,10 +12,12 @@ y = bj['move']
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
+X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                    y, 
+                                                    test_size=0.2, 
+                                                    random_state=101)
 
 knn = KNeighborsClassifier(n_neighbors=6)
-
 knn.fit(X_train,y_train)
-# KNNpred = knn.predict(X_test)
+
 
