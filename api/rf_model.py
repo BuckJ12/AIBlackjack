90bbcdbe81890d53b2e003352f62c9632e869640
Joey Wagner <joeyawagner22@gmail.com>
2024-04-12 20:03:50 -0400

    Update rf_model.py

--- api/rf_model.py
+++ api/rf_model.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
-from pred_methods import rfPrediction, standardize_inputs, standardize_input
+
 
 bj = pd.read_csv('bj_dataset.csv')
 
@@ -12,10 +12,13 @@ y = bj['move']
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
+X_train, X_test, y_train, y_test = train_test_split(X, 
+                                                    y, 
+                                                    test_size=0.2, 
+                                                    random_state=101)
 
 RFModel = RandomForestClassifier(100, random_state=101)
 RFModel.fit(X_train, y_train)
-RFpred = RFModel.predict(X_test)
+
 
 
