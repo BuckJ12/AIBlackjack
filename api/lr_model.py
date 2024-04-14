6032ef142b955ffa1de00f1a094a551b9ad98aa7
Joey Wagner <joeyawagner22@gmail.com>
2024-04-13 20:19:28 -0400

    Hyper parameterized Logistic Regression Model

--- api/lr_model.py
+++ api/lr_model.py
@@ -15,6 +15,6 @@ X = scaler.fit_transform(X)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
 
-LRModel = LogisticRegression()
+LRModel = LogisticRegression(C=1.0, penalty='l1', solver='saga')
 LRModel.fit(X_train, y_train)
 
